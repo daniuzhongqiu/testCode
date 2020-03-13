369742cb7925e4e28e9be2138e951ece67402de7
zhongqiu <1595948168@qq.com>
2020-03-13 15:02:44 +0800

    这是我第一次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    private String name;
+
+    public void test01 () {
+
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈哈");
+
+    }
+}
